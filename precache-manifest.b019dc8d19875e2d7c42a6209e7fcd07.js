2a9da2fb108675038d98c9462f0ac53245582800
Brian Hoang <bhoang35@gmail.com>
2019-01-02 20:19:23 +0000

    Updates

---
+++ precache-manifest.b019dc8d19875e2d7c42a6209e7fcd07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "90f76b9fdc792fc4a53d17223bebcfe6",
+    "url": "/nakhoda/static/media/trash.90f76b9f.svg"
+  },
+  {
+    "revision": "b940f266512f84e2bc485a3faa921832",
+    "url": "/nakhoda/static/media/switch.b940f266.svg"
+  },
+  {
+    "revision": "209683d931b4dc5afad6818873e95fc9",
+    "url": "/nakhoda/static/media/mail.209683d9.svg"
+  },
+  {
+    "revision": "13b32cd355c23d165060e3acfd5e228e",
+    "url": "/nakhoda/static/media/down.13b32cd3.svg"
+  },
+  {
+    "revision": "8ba803b24ffbc0c7b5f6",
+    "url": "/nakhoda/static/js/runtime~main.8ba803b2.js"
+  },
+  {
+    "revision": "af72222564dfd04a167b",
+    "url": "/nakhoda/static/js/main.af722225.chunk.js"
+  },
+  {
+    "revision": "82f97cf77c72f9ce4731",
+    "url": "/nakhoda/static/js/1.82f97cf7.chunk.js"
+  },
+  {
+    "revision": "af72222564dfd04a167b",
+    "url": "/nakhoda/static/css/main.aa7f7ddf.chunk.css"
+  },
+  {
+    "revision": "568414e00b99387da4ab069a476def67",
+    "url": "/nakhoda/index.html"
+  }
+];
